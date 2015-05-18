2df12cd8fb9b759a93825e1bf08ec403f4f1d422
Vlad Diachenko <apaxicko@gmail.com>
2015-05-18 23:59:21 +0400

    Create gulp.config

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	path = require('path'),
+	minifyCss = require('gulp-minify-css'),
+	livereload = require('gulp-livereload'),
+	connect = require('gulp-connect'),
+	plugins = require('gulp-load-plugins')();
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'public',
+		livereload: true
+	});
+});
+
+gulp.task('less', function () {
+  	return gulp.src('src/less/all.less')
+	    .pipe(less({
+	    	paths: [ path.join(__dirname, 'less', 'includes') ]
+	    }))
+	    .pipe(gulp.dest('public/css'))
+	    .pipe(minifyCss({compatibility: 'ie8'}))
+	    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+	gulp.watch('src/less/*.less', ['less'])
+});
+
+gulp.task('default', ['connect', 'less', 'watch']);
